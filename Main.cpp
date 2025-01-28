e31d6c62bb43321451e83b0113e222ac2a1e03ba
Nue <hipilot17@gmail.com>
2025-01-28 19:19:20 +0000

    Added comments, added the set and get methods, and added symbols for our code.
    Co-authored-by: Alexander R. and Dylan H.

--- Main.cpp
+++ Main.cpp
@@ -1,115 +1,136 @@
+//Names: Nue Lopez, Alexander R., Dylan Hamilton
+//Date: 1/28/2025
+//Assignment: Making a Card Game with OOD Group Activity
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
-//your job is to fix this object
-class Card { // Keeps a card object
+class Card {
 public:
-  //put the constructors and getters and setters here
+    //Constructors
+    Card() : value(0) {} //Set inital deck value to zero 
+    Card(const string& suit, const string& rank, int value)
+        //Int card with specific suit, rank, and value
+        : suit(suit), rank(rank), value(value) {}
+    
 
-private:
-  //put data variables here, look below to see what data variables you should have
+    // Setters
+    void set_suit(const string& suit) { //Set suit of card
+        this->suit = suit; //Use this-> to make sure it doesn't modify our main variable   
+    }
+  
+    void set_rank(const string& rank) { //Set rank of card
+        this->rank = rank;
+    }
+ //Use this-> to make sure it doesn't modify our main variable
+    void set_value(int value) { //Set value of card
+        this->value = value;
+    }
+ //Use this-> to make sure it doesn't modify our main variable 
+    //Getters 
+    string get_suit() const {
+        return suit; //returns the suit of the card
+    }
+    string get_rank() const {
+        return rank; //returns the rank of the card
+    }
+    int get_value() const {
+        return value; //returns the value of the card
+    }
+    // Method to print card details
+    void print_card() const {
+        cout << rank << " of " << suit << endl;
+    }
+
+private: //These are encapsuulated and we can't access them until we call them through the get and set methods. 
+    string suit;
+    string rank;
+    int value;
 };
 
-//define your getters and setters here
-
-//the rest of the code is working code - if you define your object above
-const string SUITS[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
-const string RANKS[] = {"2", "3",  "4",    "5",     "6",    "7",  "8",
-                        "9", "10", "Jack", "Queen", "King", "Ace"};
+//CODE ENHANCEMENT: Visual Code Upgrade: Added icons for each card suit
+const string SUITS[] = {"Spades ♠", "Hearts ♥", "Diamonds ♦", "Clubs ♣"};
+const string RANKS[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 const int VALUES[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
 
-// int DECK[52];
 Card deck[52];
 int currentCardIndex = 0;
-/*
-defines the arrays and variables related to a deck of playing cards through the
-suits, ranks, the current card index, and the deck itself.
-*/
 
 void initializeDeck() {
-  int deckIndex = 0;
-  for (int suitIndex = 0; suitIndex < 4; suitIndex++) {
-    for (int rankIndex = 0; rankIndex < 13; rankIndex++) {
-      // Card(string suit, string rank, int value);
-      deck[deckIndex++] =
-          Card(SUITS[suitIndex], RANKS[rankIndex], VALUES[rankIndex]);
+    int deckIndex = 0;
+    for (int suitIndex = 0; suitIndex < 4; suitIndex++) {
+        for (int rankIndex = 0; rankIndex < 13; rankIndex++) {
+            deck[deckIndex++] = Card(SUITS[suitIndex], RANKS[rankIndex], VALUES[rankIndex]);
+        }
     }
-  }
 }
 
 void printDeck() {
-  for (int i = 0; i < 52; i++)
-    deck[i].print_card();
+    for (int i = 0; i < 52; i++) {
+        deck[i].print_card();
+    }
 }
 
 void shuffleDeck() {
-  srand((time(0)));
-  for (int i = 0; i < 52; i++) {
-    int index = rand() % 52;
-    Card temp = deck[i]; // these next three lines are a swap function
-    deck[i] = deck[index];
-    deck[index] = temp;
-  }
+    srand(static_cast<unsigned int>(time(0)));
+    for (int i = 0; i < 52; i++) {
+        int index = rand() % 52;
+        Card temp = deck[i]; // these next three lines are a swap function
+        deck[i] = deck[index];
+        deck[index] = temp;
+    }
 }
 
 Card dealCard() { 
-  return deck[currentCardIndex++]; 
+    return deck[currentCardIndex++]; 
 }
 
 int dealInitialPlayerCards() {
-  Card card1 = dealCard();
-  Card card2 = dealCard();
-  cout << "Your cards: ";
-  card1.print_card();
-  card2.print_card();
-  //cout << "Your cards: " << RANKS[card1 % 13] << " of " << SUITS[card1 / 13]
-     //<< " and " << RANKS[card2 % 13] << " of " << SUITS[card2 / 13] << endl;
-  return card1.get_value() + card2.get_value();
-  //return cardValue(card1) + cardValue(card2);
+    Card card1 = dealCard();
+    Card card2 = dealCard();
+    cout << "Your cards: ";
+    card1.print_card();
+    card2.print_card();
+    return card1.get_value() + card2.get_value();
 }
 
 int playerTurn(int playerTotal) {
-  while (true) {
-    cout << "Your total is " << playerTotal << ". Do you want to hit or stand?"
-         << endl;
-    string action;
-    getline(cin, action);
-    if (action == "hit") {
-      Card newCard = dealCard();
-      //playerTotal += cardValue(newCard);
-      playerTotal += newCard.get_value();
-      cout << "You drew a ";
-      newCard.print_card();
-      //cout << "You drew a " << RANKS[newCard % 13] << " of "
-           //<< SUITS[newCard / 13] << endl;
-      if (playerTotal > 21) {
-        break;
-      }
-    } else if (action == "stand") {
-      break;
-    } else {
-      cout << "Invalid action. Please type 'hit' or 'stand'." << endl;
+    while (true) {
+        cout << "Your total is " << playerTotal << ". Do you want to hit or stand?" << endl;
+        string action;
+        getline(cin, action);
+        if (action == "hit") {
+            Card newCard = dealCard();
+            playerTotal += newCard.get_value();
+            cout << "You drew a ";
+            newCard.print_card();
+            if (playerTotal > 21) {
+                break;
+            }
+        } else if (action == "stand") {
+            break;
+        } else {
+            cout << "Invalid action. Please type 'hit' or 'stand'." << endl;
+        }
     }
-  }
-  return playerTotal;
+    return playerTotal;
 }
 
-
 int main() {
-  initializeDeck();
-  //printDeck();
-  shuffleDeck();
- //printDeck();
+    initializeDeck();
+    shuffleDeck();
 
-  int playerTotal = dealInitialPlayerCards();
-  cout << "The playerTotal is " << playerTotal << endl;
-  //int dealerTotal = dealInitialDealerCards();
+    int playerTotal = dealInitialPlayerCards();
+    cout << "The playerTotal is " << playerTotal << endl;
 
-  playerTotal = playerTurn(playerTotal);
-  if (playerTotal > 21) {
-    cout << "You busted! Dealer wins." << endl;
+    playerTotal = playerTurn(playerTotal);
+    if (playerTotal > 21) {
+        cout << "You BUSTED! Dealer wins. 😰" << endl; //CODE ENHANCEMENT: Visual Code Upgrade: Added a customized message when you go bust and lose.
+        return 0;
+    }
+
     return 0;
-  } 
-  
 }
